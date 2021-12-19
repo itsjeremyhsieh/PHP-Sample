73f9c89e9f8c22697e38866483efb5589c82c2bd
molly5617 <mollytsai5617@gmail.com>
2021-12-19 20:24:27 +0800

    emmm

--- Sample.php
+++ Sample.php
@@ -6,7 +6,8 @@
 
     </head>
     <body>
-        <h1>110年大學交叉查榜</h1>
+       
+        <center><h1>110年大學交叉查榜</h1></center>
         <p>請輸入您的個人資料</p>
         <form action="Sample.php" method="post">
             成績查詢方式:
@@ -14,13 +15,15 @@
             <input type="submit" name="指考" value="指考">
         </form>
         <form actime="Sample.php" method="post">
-            請輸入五科級分:
-            <input type="text" name="Chinese" value="國文">
-            <input type="text" name="English" value="英文">
-            <input type="text" name="Math" value="數學">
-            <input type="text" name="Social" value="社會">
-            <input type="text" name="Science" value="自然">
+          
+           <p>請輸入五科級分:
+            國文<input type="text" name="Chinese" value="">  
+            英文<input type="text" name="English" value="">  
+            數學 <input type="text" name="Math" value="">  
+            社會 <input type="text" name="Social" value="">  
+            自然 <input type="text" name="Science" value="">
             <input type="submit" name="submit" value="提交">
+            
     </body>
 </html>
 
