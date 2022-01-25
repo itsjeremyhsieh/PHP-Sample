835c4b5903f0ff1bb5885354c5be4b808f1a99a3
molly5617 <mollytsai5617@gmail.com>
2022-01-25 14:58:04 +0800

    未報考

--- showGSAT.php
+++ showGSAT.php
@@ -8,22 +8,22 @@
         <?php 
         echo "你以<b>學測</b>進行搜尋","<br>";
         $chi = $_POST['chinese']; 
-        if(strcmp($chi, "請選擇") )
+        if(strcmp($chi, "未報考") )
             echo "你的國文成績是<b>$chi</b>級分","<br>";
         $eng = $_POST['english']; 
-        if(strcmp($eng, "請選擇") )
+        if(strcmp($eng, "未報考") )
             echo "你的英文成績是<b>$eng</b>級分","<br>";
         $matA= $_POST['mathA']; 
-        if(strcmp($matA, "請選擇") )
+        if(strcmp($matA, "未報考") )
             echo "你的數學A成績是<b>$matA</b>級分","<br>";
         $matB= $_POST['mathB']; 
-        if(strcmp($matB, "請選擇") )
+        if(strcmp($matB, "未報考") )
             echo "你的數學A成績是<b>$matB</b>級分","<br>";
         $soc = $_POST['social']; 
-        if(strcmp($soc, "請選擇") )
+        if(strcmp($soc, "未報考") )
             echo "你的社會成績是<b>$soc</b>級分","<br>";
         $sci = $_POST['science']; 
-        if(strcmp($sci, "請選擇") )
+        if(strcmp($sci, "未報考") )
             echo "你的自然成績是<b>$sci</b>級分","<br>";
         $listen = $_POST['listening'];
         if(strcmp($listen, "non"))
