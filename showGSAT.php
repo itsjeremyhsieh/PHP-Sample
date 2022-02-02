7860e8b58d47feefe7aa32bafe5c950e69888945
itsjeremyhsieh <jeremy.life0107@gmail.com>
2022-02-02 19:22:00 +0800

    change

--- showGSAT.php
+++ showGSAT.php
@@ -117,9 +117,9 @@
                     $myallins = implode (", ", $ins);
                     echo "<div align='center'>你的興趣量表是 <b>$myallins</b>";
                 }
-
+                echo "</table>";
                 ?>
-          
+                
                 <br><br>
             
                 <?php 
@@ -128,20 +128,7 @@
                         echo "MySQL資料庫連線錯誤!";
                     }
                     else{
-                    /* $sql = "UPDATE 大學入學門檻 U
-                                SET U.Chinese = (Select Chinese FROM 110五標分數 S WHERE U.Chinese = S.五標),
-                                U.English = (Select English FROM 110五標分數 S WHERE U.English = S.五標),
-                                U.Math = (Select Math FROM 110五標分數 S WHERE U.Math = S.五標),
-                                U.Social = (Select Social FROM 110五標分數 S WHERE U.Social = S.五標),
-                                U.Science = (Select Science FROM 110五標分數 S WHERE U.Science = S.五標)"; 
-
-                        if ($link->query($sql) === TRUE) {
-                            if($link-> query("SELECT IFNULL(")
-                        
-
-                            } else {
-                            echo "Error updating record: " . $link->error;
-                            }*/
+                   
                         $sql = "SELECT * FROM 大學入學門檻 U WHERE $chi >= U.Chinese AND $eng >= U.English AND $matA >= U.Math AND $soc >= U.Social AND $sci >= U.Science AND $listennum >= U.Listening";
                         $result = mysqli_query($link, $sql);
                         echo "<div align='center'><table border = 1>";
@@ -152,21 +139,7 @@
                         echo "</table>";
                     }
                 ?>
-            
-        <!--
-                您的落點分析結果如下表所示：
-                <table  wide=”200” height=”800” border =”3”>
-                <tr>
-                <td align="center"><b>順位</b></td>
-                <td align="center"><b>校系代碼</b></td>
-                <td align="center"><b>校系名稱</b></td>   
-                <td align="center"><b>通過機率</b></td>
-                <td align="center"><b>甄試日期</b></td></tr>
-                <tr><td> 1 </td><td>023152</td><td> 國立彰化師範大學資訊工程學系</td><td>☆☆☆☆☆</td><td>無</td></tr> 
-                <tr><td> 2 </td><td>001572</td><td> 國立台灣大學電機工程學系</td><td>☆</td><td>3/10</td></tr> 
-                </p>
-                </table> -->
-                
+
                 <h2>相關連結</h2>
                 <a href='https://www.ceec.edu.tw/'>大考中心首頁</a>
         </div>
