4e50832a8e8a89e6e0f6eab2c3c42f240465c6ce
molly5617 <mollytsai5617@gmail.com>
2022-07-07 10:04:40 +0800

    .

---
+++ 落點分析/result.php
@@ -0,0 +1,450 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Site Metas -->
+    <title>Perfect - Responsive HTML5 Template</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="#" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="#" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Pogo Slider CSS -->
+    <link rel="stylesheet" href="css/pogo-slider.min.css" />
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css" />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css" />
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="inner_page" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+
+    <!-- LOADER -->
+    <div id="preloader">
+        <div class="loader">
+            <img src="images/loader.gif" alt="#" />
+        </div>
+    </div>
+    <!-- end loader -->
+    <!-- END LOADER -->
+
+    <!-- Start header -->
+    <header class="top-header">
+        <nav class="navbar header-nav navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="image"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd" aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-wd">
+                    <ul class="navbar-nav">
+						<li><a class="nav-link active" href="index.html">Home</a></li>
+                        <li><a class="nav-link" href="GSAT.html">學測</a></li>
+                        <li><a class="nav-link" href="AST.html">分科測驗</a></li>
+                        <li><a class="nav-link" href="register.html">會員註冊</a></li>
+						<li><a class="nav-link" href="news.html">更新消息</a></li>
+						<li><a class="nav-link" href="contact.html">聯絡我們</a></li>
+					</ul>
+                </div>
+                <div class="search-box">
+                    <input type="text" class="search-txt" placeholder="Search">
+                    <a class="search-btn">
+                        <img src="images/search_icon.png" alt="#" />
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- End header -->
+
+	<!-- section -->
+	
+	<section class="inner_banner">
+	  <div class="container">
+	      <div class="row">
+		      <div class="col-12">
+			     <div class="full">
+				     <h3>結果分析</h3>
+				 </div>
+			  </div>
+		  </div>
+	  </div>
+	</section>
+	
+	<!-- end section -->
+   
+	<!-- section -->
+    <div class="section layout_padding contact_section" style="background:#f6f6f6;">
+        <div class="container">
+               <div class="row">
+				<div class="col-xs-8">
+					<table align="center" style="width: 95%;text-align: center;border-collapse:collapse;">
+						<tr>
+							<td colspan="6" align="center" bgcolor="#E3E3E3" style="border: 1px solid #E3E3E3;"><span style="font-size: 16px;font-weight: bold;">優勢科目組合排名</span></td>
+						</tr>
+						<tr>
+							<td bgcolor="#F4FF83" style="border: 1px  solid #E3E3E3;">排名</td>
+																								<td style="color: #FF3299;border: 1px solid #E3E3E3;">1</td>
+																									<td style="color: #FF3299;border: 1px solid #E3E3E3;">2</td>
+																									<td style="color: #FF3299;border: 1px  solid #E3E3E3;">3</td>
+																									<td style="color: #FF3299;border: 1px solid #E3E3E3;">4</td>
+																									<td style="color: #FF3299;border: 1px  solid #E3E3E3;">5</td>
+																
+							<!--td bgcolor="#F4FF83" style="border: 1px solid #E3E3E3;">全國累積人數百分比</td-->
+						</tr>
+						   <tr>
+							   <td bgcolor="#F9FFBF" style="border: 1px solid #E3E3E3;">科目組合</td>
+																	  <td style="border: 1px solid #E3E3E3;">英文、數學A、自然</td>
+																	  <td style="border: 1px solid #E3E3E3;">英文、數學B、自然</td>
+																	  <td style="border: 1px solid #E3E3E3;">英文、社會、自然</td>
+																	  <td style="border: 1px solid #E3E3E3;">英文、數學B、社會</td>
+																	  <td style="border: 1px solid #E3E3E3;">英文、數學A、社會</td>
+															  </tr>
+					</table>
+				</div>
+
+
+
+
+			
+				
+				
+				<table style="width:1100px;line-height:3px;border:2px #D4D4D4 solid;"  align=center cellpadding="10" border=1 >
+					<tr>
+					<td  colspan=64  bgcolor="#19CAAD" style="width: 50%; border:1px #D4D4D4 solid;" align=center cellpadding="8" border="1"><b>我的成績</b></td>
+					</tr>
+				
+							<tr>
+							<td   colspan=4   bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>科目</td>
+							<td   colspan=4 bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>國文</td>
+							<td   colspan=4 bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>英文</td>
+							<td   colspan=4 bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>數學A</td>
+							<td   colspan=4 bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>數學B</td>
+							<td   colspan=4 bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>社會</td>
+							<td   colspan=4 bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>自然</td>
+							<td   colspan=4 bgcolor=#8CC7B5 style="border:1px #D4D4D4 solid;" align=center cellpadding="8" border=1>聽力</td>
+							</tr>
+							
+							<?php 
+						   
+							
+							echo  "<tr><td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>級分</td>";
+							$chi = $_POST['chinese']; 
+							if(strcmp($chi, "未報考") )
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$chi</td>";
+							else{
+								$chi = 0;
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>未報考</td>";
+							}
+							$eng = $_POST['english']; 
+							if(strcmp($eng, "未報考") )
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$eng</td>";
+							else{
+								$eng = 0;
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>未報考</td>";
+							}
+							$matA= $_POST['mathA']; 
+							if(strcmp($matA, "未報考") )
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$matA</td>";
+							else{
+								$matA = 0;
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>未報考</td>";
+							}
+							$matB= $_POST['mathB']; 
+							if(strcmp($matB, "未報考") )
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$matB</td>";
+							else{
+								$matB = 0;
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>未報考</td>";
+							}
+							$soc = $_POST['social']; 
+							if(strcmp($soc, "未報考") )
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$soc</td>";
+							else{
+								$soc = 0;
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>未報考</td>";
+							}
+							$sci = $_POST['science']; 
+							if(strcmp($sci, "未報考") )
+								echo "<td colspan=4 align='center' bgcolor=#A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$sci</td>";
+							else{
+								$sci = 0;
+								echo "<td colspan=4 align='center' bgcolor=#A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>未報考</td>";
+							}
+							$listen = $_POST['listening'];
+							if(strcmp($listen, "non"))
+							{
+								if($listen == "A")
+									$listennum = 3;
+								else if($listen == "B")
+									$listennum = 2;
+								else if($listen == "C")
+									$listennum = 1;
+								else if($listen == "F")
+									$listennum = 0;
+									echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$listen</td>";
+							}
+							else{
+								$listennum = 0;
+								echo "<td colspan=4 align='center' bgcolor= #A0EEE1 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>未報考</td>";
+							}
+							echo "</tr>";
+							echo "<tr><td  colspan=4 align='center' bgcolor=#BEE7E9 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>換算去年級分</td></tr>";
+			
+			
+							//
+							echo "<td  colspan=4 align='center' bgcolor=#BEEDC7 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>你的興趣量表</td>";
+							if(isset( $_POST ["interest"]))
+							{
+								$ins = $_POST ["interest"];
+								echo '<br>';  
+								$myallins = implode (", ", $ins);
+								echo "<td colspan=28 align='center' bgcolor=#BEEDC7	 style= 'border:1px #D4D4D4 solid'; align=center cellpadding=10 border=1>$myallins</td>";
+								
+							}
+						  
+							?>
+					</table>
+
+
+
+
+
+
+
+					您的落點分析結果如下表所示：
+        <table  wide=”200” height=”800” border =”3”>
+        <tr>
+        <td align="center"><b>順位</b></td>
+        <td align="center"><b>校系代碼</b></td>
+        <td align="center"><b>校系名稱</b></td>   
+        <td align="center"><b>通過機率</b></td>
+        <td align="center"><b>甄試日期</b></td></tr>
+        <tr><td> 1 </td><td>023152</td><td> 國立彰化師範大學資訊工程學系</td><td>☆☆☆☆☆</td><td>無</td></tr> 
+        <tr><td> 2 </td><td>001572</td><td> 國立台灣大學電機工程學系</td><td>☆</td><td>3/10</td></tr> 
+        </p>
+        </table>
+
+		
+
+
+
+                
+               </div>			  
+           </div>
+        </div>
+	<!-- end section -->
+  <!-- Start Footer -->
+  <footer class="footer-box">
+	<div class="container">
+	
+	   <div class="row">
+	   
+		  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+			 <div class="footer_blog">
+				<div class="full margin-bottom_30">
+				   <img src="images/footer_logo.png" alt="#" />
+				 </div>
+				 <div class="full white_fonts">
+					<p>梅子黃時，道了無數的珍重，寫了無數的祝福，合了無數張留影，隨處可見夏日的繁茂。我們以為是終點，其實只是一個攜帶祝福的開端。 </p>
+				 </div>
+			 </div>
+		  </div>
+		  
+		  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+			   <div class="footer_blog footer_menu white_fonts">
+						<h3>快速連結</h3>
+						<ul> 
+						  <li><a href="#">> Home</a></li>
+						  <li><a href="#">> 學測</a></li>
+						  <li><a href="#">> 分科測驗</a></li>
+						  <li><a href="#">> 會員註冊</a></li>
+						  <li><a href="#">> 更多消息</a></li>
+						  <li><a href="#">> 聯絡我們</a></li>
+						</ul>
+					 </div>
+			 </div>
+			 
+			 <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+			 <div class="footer_blog full white_fonts">
+						 <h3>訂閱電子報</h3>
+						 <p>訂閱電子報以獲得更多消息</p>
+						 <div class="newsletter_form">
+							<form action="index.html">
+							   <input type="email" placeholder="Email" name="#" required />
+							   <button>送出</button>
+							</form>
+						 </div>
+					 </div>
+				</div>	 
+		  
+		  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+			 <div class="footer_blog full white_fonts">
+						 <h3>Contact us</h3>
+						 <ul class="full">
+						   <li><img src="images/i5.png"><span>500彰化縣彰化市進德路1號<br>寶山校區</span></li>
+						   <li><img src="images/i6.png"><span>csie@cc2.ncue.edu.tw</span></li>
+						   <li><img src="images/i7.png"><span>04-7232105 分機8401~8405</span></li>
+						 </ul>
+					 </div>
+				</div>	 
+		  
+	   </div>
+	
+	</div>
+</footer>
+<!-- End Footer -->
+
+<div class="footer_bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<p class="crp">© 國立彰化師範大學資訊工程學系</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+    <a href="#" id="scroll-to-top" class="hvr-radial-out"><i class="fa fa-angle-up"></i></a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.pogo-slider.min.js"></script>
+    <script src="js/slider-index.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/isotope.min.js"></script>
+    <script src="js/images-loded.min.js"></script>
+    <script src="js/custom.js"></script>
+	<script>
+	/* counter js */
+
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+		
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+			
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+			
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+			
+			$self.data('countTo', data);
+			
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+			
+			// initialize the element with the starting value
+			render(value);
+			
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+				
+				render(value);
+				
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+				
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+					
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+			
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.html(formattedValue);
+			}
+		});
+	};
+	
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+	
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+jQuery(function ($) {
+  // custom formatting example
+  $('.count-number').data('countToOptions', {
+	formatter: function (value, options) {
+	  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+	}
+  });
+  
+  // start all the timers
+  $('.timer').each(count);  
+  
+  function count(options) {
+	var $this = $(this);
+	options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+	$this.countTo(options);
+  }
+});
+	</script>
+</body>
+
+</html>
