4ff85af31209a205dcff454ae07c750b4f32a379
itsjeremyhsieh <jeremy.life0107@gmail.com>
2022-07-18 13:01:41 +0800

    form validation

--- 落點分析/loginfunc.php
+++ 落點分析/loginfunc.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
 
-$link = mysqli_connect('120.107.172.131', 'admin1', 'admin123456!', 'website_database') // 建立MySQL的資料庫連結
+
+$db_host        = '120.107.172.131';
+$db_user        = 'admin1';
+$db_pass        = 'admin123456!';
+$db_database    = 'website_database'; 
+$db_port        = '3306';
+$link = mysqli_connect($db_host,$db_user,$db_pass,$db_database,$db_port);
 or die("無法開啟MySQL資料庫連結!<br>");
 $sql = "select * from member";
 // 送出編碼的MySQL指令
