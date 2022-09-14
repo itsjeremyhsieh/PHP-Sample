643217802f02c83aa7eb653500e2b27d16d796fd
itsjeremyhsieh <jeremy.life0107@gmail.com>
2022-09-15 01:26:37 +0800

    database

--- 落點分析/savetmp2.php
+++ 落點分析/savetmp2.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 
-/*$link = mysqli_connect('localhost', 'admin', 'NCUEcsie!@', 'website_database') 
+$link = mysqli_connect('localhost', 'admin', 'NCUEcsie!@', 'website_database') 
 or die("無法開啟MySQL資料庫連結!<br>");
-$sql = "select * from member";
 // 送出編碼的MySQL指令
 mysqli_query($link, 'SET CHARACTER SET utf8');
-mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");*/
+mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");
 
 $school_order = intval($_POST['school_order']);
 $percentage_order = intval($_POST['percentage_order']);
@@ -50,6 +49,7 @@ for($i = 0 ; $i < count($result) ; $i++)
     echo $result[$i] . " ";
 }
 echo count($result);
-
+$sql = "insert into searchrecord values ('" . $result[0] . "','" . $result[1] . "','" . $result[2] . "','" . $result[3] . "','" . $result[4] . "','" . $result[5] . "','" . $result[6]. "','" . $result[7]. "','" . $result[8]. "','" . $result[9]. "','" . $result[10]. "','" . $result[11]. "','" . $result[12]. "','" . $result[13]. "','" . $result[14]. "','" . $result[15]. "','" . $result[16]. "','" . $result[17]. "','" . $result[18]. "','" . $result[19]. "','" . $result[20]. "','" . $result[21]. "','" . $result[22]. "','" . $result[23]. "','" . $result[24]. "','" . $result[25]. "','" . $result[26]. "','" . $result[27]. "','" . $result[28]. "','" . $result[29]. "','" . $result[30]. "','" . $result[31]. "','" . $result[32]. "')";
+$result = mysqli_query($link, $sql);
 //header("Location: index.php");
 ?>
