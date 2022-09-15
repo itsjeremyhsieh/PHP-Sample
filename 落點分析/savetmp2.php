faec0cf07319c72d29db93d37690f37a356d1127
itsjeremyhsieh <jeremy.life0107@gmail.com>
2022-09-15 19:52:18 +0800

    id

--- 落點分析/savetmp2.php
+++ 落點分析/savetmp2.php
@@ -7,6 +7,7 @@ or die("無法開啟MySQL資料庫連結!<br>");
 mysqli_query($link, 'SET CHARACTER SET utf8');
 mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");
 
+
 $school_order = intval($_POST['school_order']);
 $percentage_order = intval($_POST['percentage_order']);
 $field_order = intval($_POST['field_order']);
@@ -42,14 +43,16 @@ $s = $_SESSION['s'];
 $e = $_SESSION['e'];
 $c = $_SESSION['c'];
 
-
-$result = array($chinese, $english, $matha, $mathb, $socialstu, $sciencestu, $listening, $r, $i, $a, $s, $e, $c, $school_order, $percentage_order, $field_order, $education, $art, $social, $business, $science, $cs, $med, $constructure, $agri, $service, $other, $location_order, $north, $central, $south, $east, $island);
-for($i = 0 ; $i < count($result) ; $i++)
+$result = mysqli_query($link, "SELECT * FROM searchrecord");
+$num = mysqli_num_rows($result); //查詢結果筆數
+$num = $num + 1;
+$result = array($num, $chinese, $english, $matha, $mathb, $socialstu, $sciencestu, $listening, $r, $i, $a, $s, $e, $c, $school_order, $percentage_order, $field_order, $education, $art, $social, $business, $science, $cs, $med, $constructure, $agri, $service, $other, $location_order, $north, $central, $south, $east, $island);
+/*for($i = 0 ; $i < count($result) ; $i++)
 {
     echo $result[$i] . " ";
 }
-echo count($result);
-$sql = "insert into searchrecord values ('" . $result[0] . "','" . $result[1] . "','" . $result[2] . "','" . $result[3] . "','" . $result[4] . "','" . $result[5] . "','" . $result[6]. "','" . $result[7]. "','" . $result[8]. "','" . $result[9]. "','" . $result[10]. "','" . $result[11]. "','" . $result[12]. "','" . $result[13]. "','" . $result[14]. "','" . $result[15]. "','" . $result[16]. "','" . $result[17]. "','" . $result[18]. "','" . $result[19]. "','" . $result[20]. "','" . $result[21]. "','" . $result[22]. "','" . $result[23]. "','" . $result[24]. "','" . $result[25]. "','" . $result[26]. "','" . $result[27]. "','" . $result[28]. "','" . $result[29]. "','" . $result[30]. "','" . $result[31]. "','" . $result[32]. "')";
+echo count($result);*/
+$sql = "insert into searchrecord values ('" . $result[0] . "','" . $result[1] . "','" . $result[2] . "','" . $result[3] . "','" . $result[4] . "','" . $result[5] . "','" . $result[6]. "','" . $result[7]. "','" . $result[8]. "','" . $result[9]. "','" . $result[10]. "','" . $result[11]. "','" . $result[12]. "','" . $result[13]. "','" . $result[14]. "','" . $result[15]. "','" . $result[16]. "','" . $result[17]. "','" . $result[18]. "','" . $result[19]. "','" . $result[20]. "','" . $result[21]. "','" . $result[22]. "','" . $result[23]. "','" . $result[24]. "','" . $result[25]. "','" . $result[26]. "','" . $result[27]. "','" . $result[28]. "','" . $result[29]. "','" . $result[30]. "','" . $result[31]. "','" . $result[32].  "','" . $result[33]. "')";
 $result = mysqli_query($link, $sql);
 //header("Location: index.php");
 ?>
